826708da5b02d8f2cfe561b64c431494e69bb1d0
Muxindong <751795623@qq.com>
2023-01-11 20:50:05 +0800

    1月11日版本1（解决jpa链接数据库问题）

--- springboot/src/main/java/com/example/springbooot/entity/User.java
+++ springboot/src/main/java/com/example/springbooot/entity/User.java
@@ -4,45 +4,14 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-//@Data
+@Data
+@Table(name="users")
 public class User {
     @Id
-    private Integer id;
-    private String username;
-    private String password;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public User() {
-    }
-
-    public User(Integer id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
+    private Integer id1;
+    private String username1;
+    private String password1;
 }
